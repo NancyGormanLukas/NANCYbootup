da04c4b234dc4cd29be3a3dd4d0e19afbadd78cc
nancylukas <nlukas13@optonline.net>
2016-08-16 00:28:33 -0400

    new files

---
+++ seeders/20160816033611-unnamed-seeder.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+
+      //Add altering commands here.
+      //Return a promise to correctly handle asynchronicity.
+      return queryInterface.bulkInsert('Categories', [{
+        title: 'Javascript',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        title: 'HTML',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        title: 'CSS',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }, {
+        title: 'Database',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+  }
+};
--- server.js
+++ server.js
@@ -1,15 +1,34 @@
 var express = require('express');
-var BodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 var handlebars = require('express-handlebars');
+var sequelize = require('sequelize');
+var Posts = require('./models')['Posts'];
+var Categories = require('./models')['Categories'];
+var Comments = require('./models')['Comments'];
 
-var Posts= require('./models')['Posts'];
-Posts.sync();
+var models  = require('./models');
+var sequelizeConnection = models.sequelize
+
+
+// We run this query so that we can drop our tables even though they have foreign keys
+sequelizeConnection.query('SET FOREIGN_KEY_CHECKS = 0')
+
+// make our tables
+// note: force:true drops the table if it already exists
+.then(function(){
+	return sequelizeConnection.sync({})
+}).then(function() {
+	Categories.findAll({}).then(function(results) {
+		console.log(results);
+		app.locals.categories = results;
+	});
+})
 
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
-app.use(BodyParser.urlencoded({
+app.use(bodyParser.urlencoded({
 	extended: false
 }));
 
@@ -22,56 +41,103 @@ app.set('view engine', 'handlebars');
 //home page
 app.get('/', function(req, res) {
 
-	Posts.findAll({}).then(function(result) {
+	Posts.findAll({
+		order: 'score DESC'
+	}).then(function(result) {
 		console.log(result);
 		return res.render('index', {
 			posts: result
 		});
 	});
-	
 
 });
+
+
+app.get('/categories/:category', function(req, res) {
+	var categoryName = req.params.category;
+
+	Categories.findAll({
+		where: {
+			title: categoryName
+		},
+		include: [{model: models.Posts, required: true}]
+	}).then(function(result) {
+		console.log('rending category');
+		return res.render('categories', {
+			categoryName: categoryName,
+			category: result
+		});
+	});
+});
+
 //form page
 app.get('/new-post', function(req, res) {
 	res.render('new');
-	
 });
+
+
+app.post('/:postId/comment', function(req, res) {
+	var body = req.body;
+	console.log('body', body);
+	var comment = body.comment;
+
+	Comments.create({
+		comment: comment,
+		PostId: parseInt(req.params.postId, 10)
+	}).then(function(data) {
+		res.redirect('/posts/' + req.params.postId);
+	});
+});
+
+
 app.post('/new-post', function(req, res) {
 	var body = req.body;
+
 	//create the post in the database
 	Posts.create({
 		title: body.title,
 		url: body.url,
 		image: body.image,
 		score: 0,
+		CategoryId: parseInt(body.category, 10),
 		description: body.description
-
-
-	}).then(function(data){
+	}).then(function(data) {
 		console.log('data', data);
-	//redirect to the posts/:id page
+		//redirect to the posts/:id page
 		res.redirect('/posts/' + data.dataValues.id);
 	});
 });
 
 
-
 app.get('/posts/:id', function(req, res) {
 	var id = req.params.id;
 	Posts.findOne({
-		where:{
+		where: {
 			id: id
-		}
-	}).then(function(post){
+		},
+		include: [models.Comments]
+	}).then(function(post) {
 		console.log('post', post);
-		res.render('post');
+		res.render('post', {
+			post: post
+		});
 	});
 });
 
+app.post('/posts/:id/:vote', function(req, res) {
+	var operation = '+';
+	if (req.params.vote === 'downvote') {
+		operation = '-';
+	}
 
-
+	Posts.update({
+		score: sequelize.literal('score ' + operation + ' 1')},
+	{where: {id: req.params.id}}).then(function(post) {
+		res.end();
+	});
+});
 
 var port = process.env.PORT || 3000;
-app.listen(port, function(){
-	console.log('connected to ', port);
+app.listen(port, function() {
+	console.log('connected to', port);
 });
